06ea934646af4757ed0bd6cb11d497f9e2e93114
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-07-25 21:09:34 +0530

    Create CONCATSORT.cpp

---
+++ JULY222/CONCATSORT.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include<cstring>
+#include <stack>
+
+using namespace std;
+
+void fun(){
+    int x, t1 ,t2;
+    cin>>x;
+
+    int ar[x];
+    int arr[x];
+    for(int i=0;i<x;i++){
+        cin>>ar[i];
+        arr[i]=ar[i];
+    }
+    
+    sort(arr, arr + x);
+    t1=arr[0];
+
+    for(int i=0;i<x;i++){
+        if(arr[i]!=ar[i]){
+            t2=ar[i];
+        }
+    }
+    for(int i=0;i<x;i++){
+        if(t1<=ar[i] && ar[i]<t2){
+            t1=ar[i];
+            
+        }else if( t2<=ar[i]){
+            t2=ar[i];
+            
+        }else{
+            cout<<"NO"<<endl;
+            return;
+        }
+    }
+    cout<<"YES"<<endl;
+
+    
+}
+
+
+int main() {
+    int n;
+    cin>> n;
+    for(int i =0;i<n;i++){
+        fun();
+    }
+	return 0;
+}
+
