9243ec79893b2f86b80363984616d6b2775cd79e
kartik-gupta-ij <kartikgupta7267@gmail.com>
2022-07-25 21:33:32 +0530

    Create two-sum.cpp

---
+++ leetcode/two-sum.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;i<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                    ans.push_back(i);
+                    ans.push_back(j);
+                    return ans;
+                }
+            }
+        }
+    }
+};
